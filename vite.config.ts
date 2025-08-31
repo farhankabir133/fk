0816306023a6389282ad1b2bc783addb0b8e1e68
A. S. M. Farhan Kabir Redoy <102225132+farhankabir133@users.noreply.github.com>
2025-09-01 03:21:05 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,30 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  base: '/fk/', // Use your repo name if deploying to farhankabir133.github.io/fk
+});
+
+
+
+
+//import { defineConfig } from 'vite';
+//import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+//export default defineConfig({
+  //plugins: [react()],
+  //optimizeDeps: {
+    //exclude: ['lucide-react'],
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
-      },
+  //server: {
+    //proxy: {
+      //'/api': {
+        //target: 'http://localhost:5001',
+        //changeOrigin: true,
+    //  },
     },
   },
 });
